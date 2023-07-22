f2ceb9f78129ef73946455c4d60621ba9cb46a30
Teemu Sillantaus <10772816+silte@users.noreply.github.com>
2023-07-22 21:40:48 +0300

    Update icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
-import './globals.css'
+
 import { Inter } from 'next/font/google'
+
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/workout-planner.png" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
